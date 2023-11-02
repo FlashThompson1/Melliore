38b3dce257b59f5df724240161976ebb0095bf90
Zayniddin <89527543+FlashThompson1@users.noreply.github.com>
2023-11-02 20:04:33 +0500

    Add files via upload

---
+++ funct.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("main").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.height= "0";
+}
